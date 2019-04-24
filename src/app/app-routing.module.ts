d6b770a09959988c1a23bbc87ac447eb54f66632
mefy-Priyanka <priyanka.saha@mefy.care>
2019-04-24 22:57:50 +0530

    component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'sidenav', component: SidenavComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  
 ];
 @NgModule({
   declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FleetComponent } from './dashboard/fleet/fleet.component';
+import { ContractorComponent } from './contractor/contractor.component';
+import { ContractorcreateComponent } from './contractorcreate/contractorcreate.component';
+import { ContractoruserComponent } from './contractoruser/contractoruser.component';
+import { UserComponent } from './user/user.component';
+import { OrderComponent } from './dashboard/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SidenavComponent
+    SidenavComponent,
+    DashboardComponent,
+    FleetComponent,
+    ContractorComponent,
+    ContractorcreateComponent,
+    ContractoruserComponent,
+    UserComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
