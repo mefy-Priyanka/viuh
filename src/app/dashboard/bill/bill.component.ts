6fc3eb43f07928a5fffd1d385b28a6d9b2551484
mefy-brij <brij.singh@mefy.care>
2019-06-10 17:03:16 +0530

    bill

--- src/app/dashboard/bill/bill.component.ts
+++ src/app/dashboard/bill/bill.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
+
+import { ToastrService } from 'ngx-toastr';
+import { CompanyService } from 'src/app/service/company.service';
 
 @Component({
   selector: 'app-bill',
@@ -7,11 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillComponent implements OnInit {
 public  bill:boolean=true;
-  constructor() { }
+billForm: FormGroup;
+  constructor(private fb: FormBuilder, private toastr: ToastrService,private companyService:CompanyService) { }
 
   ngOnInit() {
+    this.billForm = this.fb.group({
+      Work_order: '',
+      customer: '',
+      invoice_date: '',
+      terms: '',
+      due_date: '',
+      sub_total: '',
+      adjustment: 0,
+      periodstart: '',
+      periodend: '',
+      reverse_change: '',
+      gstamount: 0,
+      gstrate: 0,
+      amount: '',
+      arraydata: this.fb.array([])
+    })
+
+    // this.billForm.valueChanges.subscribe(() => {
+    //   this.onmyFormValuesChanged();
+    // });
+    this.addPhone();
   }
+
   createbill(){
     this.bill=!this.bill;
+  }
+  get phoneForms() {
+    return this.billForm.get('arraydata') as FormArray
+  }
+  addPhone() {
+
+    const phone = this.fb.group({
+      serial_number: [this.phoneForms.value.length + 1],
+      cosignmentId: [],
+      description: [],
+      amount: [0]
+    })
+    this.phoneForms.push(phone);
+
   }
 }
