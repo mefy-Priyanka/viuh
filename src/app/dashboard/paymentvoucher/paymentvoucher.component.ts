75c20793d0ee66ac16ddf16534e4c70c1a02809a
mefy-Priyanka <priyanka.saha@mefy.care>
2019-07-10 11:56:18 +0530

    ui

---
+++ src/app/dashboard/paymentvoucher/paymentvoucher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentvoucher',
+  templateUrl: './paymentvoucher.component.html',
+  styleUrls: ['./paymentvoucher.component.css']
+})
+export class PaymentvoucherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
