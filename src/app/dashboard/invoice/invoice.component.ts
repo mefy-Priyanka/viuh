569f90bffa624c8432d736826f6f284f026ca0b1
mefy-brij <brij.singh@mefy.care>
2019-08-21 17:53:38 +0530

    rr

--- src/app/dashboard/invoice/invoice.component.ts
+++ src/app/dashboard/invoice/invoice.component.ts
@@ -14,12 +14,16 @@ import { SharedService } from 'src/app/service/shared.service';
 })
 export class InvoiceComponent implements OnInit {
   myForm: FormGroup;
-  consigmentDetail=[];
-  contactlist=[];
-  worklist=[];
+  consigmentDetail = [];
+  contactlist = [];
+  worklist = [];
   firstaccountid: any;
   fleetaccount: any;
-  constructor(private fb: FormBuilder,   private SharedService :SharedService, private userService: UserService, private toastr: ToastrService,private companyService:CompanyService) { }
+  ratelist: any = [];
+  selectedconsignment: any;
+  totalval: number;
+  customername: any;
+  constructor(private fb: FormBuilder, private SharedService: SharedService, private userService: UserService, private toastr: ToastrService, private companyService: CompanyService) { }
 
   ngOnInit() {
     this.myForm = this.fb.group({
@@ -49,9 +53,93 @@ export class InvoiceComponent implements OnInit {
     this.addPhone();
     this.getConsignmentList();
     this.customerList();
-    this.customerList(); 
+    this.customerList();
     this.getwork();
-    this.get()
+    this.get();
+    this.getrate()
+  }
+
+
+  change() {
+
+    this.totalval = 0;
+    var temp: any = [];
+    console.log(this.ratelist);
+    var result = jQuery('.switch-input').is(':checked') ? true : false;
+    for (var i = 0; i < this.ratelist.length; i++) {
+      if (this.ratelist[i].truck_confg == this.selectedconsignment.truckconfig) {
+        if (result == this.ratelist[i].within_state) {
+          if (this.ratelist[i].price_type == this.selectedconsignment.price_type) {
+            if (((new Date(this.ratelist[i].effactive_date_from).getTime()) <= (new Date(this.selectedconsignment.consignment_date).getTime()) && (new Date(this.ratelist[i].effactive_date_to).getTime()) >= (new Date(this.selectedconsignment.consignment_date).getTime()))) {
+              console.log('config and price type and date and state matched')
+              temp.push(this.ratelist[i]);
+              console.log(temp)
+            }
+          }
+        }
+      }
+    }
+
+    if (this.selectedconsignment.truckconfig == 12 || this.selectedconsignment.truckconfig == 19 || this.selectedconsignment.truckconfig == 20 || this.selectedconsignment.truckconfig == 24) {
+      console.log(temp[temp.length - 1])
+      if (temp[temp.length - 1]) {
+        if (temp[temp.length - 1].price_type == 'fdz') {
+          console.log(parseFloat(this.selectedconsignment.truckconfig), parseFloat(temp[temp.length - 1].rate), parseFloat(temp[temp.length - 1].to_km))
+          this.totalval = parseFloat(this.selectedconsignment.truckconfig) * parseFloat(temp[temp.length - 1].rate);
+        }
+        else if (temp[temp.length - 1].price_type == 'bfdz') {
+          console.log(parseFloat(this.selectedconsignment.truckconfig), parseFloat(temp[temp.length - 1].rate), parseFloat(this.selectedconsignment.distance))
+          this.totalval = parseFloat(this.selectedconsignment.truckconfig) * parseFloat(temp[temp.length - 1].rate) * parseFloat(this.selectedconsignment.distance);
+        }
+      }
+
+
+
+    }
+    else if (this.selectedconsignment.truckconfig == 306 || this.selectedconsignment.truckconfig == 450) {
+      if (temp[temp.length - 1]) {
+        if (temp[temp.length - 1].price_type == 'fdz') {
+          this.totalval = parseFloat(this.selectedconsignment.truckconfig) * parseFloat(temp[temp.length - 1].rate);
+        }
+        else if (temp[temp.length - 1].price_type == 'bfdz') {
+          this.totalval = parseFloat(this.selectedconsignment.truckconfig) * parseFloat(temp[temp.length - 1].rate) * parseFloat(this.selectedconsignment.distance) * 2;
+        }
+      }
+
+    }
+    if (this.customername.match('emami')) {
+      var net = 0;
+      for (var i = 0; i < this.selectedconsignment.consignment.length; i++) {
+        
+        net = net + parseInt(this.selectedconsignment.consignment[i].net_wt)
+      }
+      console.log( net)
+      this.totalval = parseFloat(this.selectedconsignment.freight) * net;
+    }
+    // alert(this.totalval)
+    this.selectedconsignment.amount = this.totalval
+    console.log(this.totalval)
+    if (this.totalval == 0) {
+      let xx = confirm('there is no rate defined')
+      if (xx) {
+
+      }
+    }
+    else {
+      alert(this.selectedconsignment.amount)
+
+    }
+
+
+  }
+  changeconsigner(id) {
+    for (var i = 0; i < this.consigmentDetail.length; i++) {
+      if (this.consigmentDetail[i]._id == id) {
+        this.selectedconsignment = this.consigmentDetail[i];
+        this.change()
+        return;
+      }
+    }
   }
   getConsignmentList() {
     this.userService.consignmentList(localStorage.getItem('SuperAdmin')).subscribe(data => {
@@ -67,29 +155,29 @@ export class InvoiceComponent implements OnInit {
       })
   }
 
-  getwork(){
-    let some:any;
-  
-    this.companyService.getworkorder(localStorage.getItem('SuperAdmin')).subscribe(result=>{
-      some=result
+  getwork() {
+    let some: any;
+
+    this.companyService.getworkorder(localStorage.getItem('SuperAdmin')).subscribe(result => {
+      some = result
       console.log(some.result);
-      this.worklist=some.result
+      this.worklist = some.result
     },
-    err=>{
-      console.log(err);
-      this.toastr.error('oops','work order creation failed')
-  
-    })
+      err => {
+        console.log(err);
+        this.toastr.error('oops', 'work order creation failed')
+
+      })
   }
 
-  
+
   customerList() {
-    let data={
-      id:localStorage.getItem('SuperAdmin'),
-      contact_type:'customer'
+    let data = {
+      id: localStorage.getItem('SuperAdmin'),
+      contact_type: 'customer'
     }
     this.companyService.getcustomer(data).subscribe(data => {
-     
+
       let result: any = {}
       result = data;
       this.contactlist = result.result
@@ -131,7 +219,7 @@ export class InvoiceComponent implements OnInit {
     this.myForm.value.sgstamount = (this.myForm.value.sgstrate * adjustedval) / 100;
 
 
-    this.myForm.value.amount = this.myForm.value.igstamount + adjustedval+this.myForm.value.sgstamount+this.myForm.value.cgstamount;
+    this.myForm.value.amount = this.myForm.value.igstamount + adjustedval + this.myForm.value.sgstamount + this.myForm.value.cgstamount;
 
   }
   addPhone() {
@@ -145,7 +233,18 @@ export class InvoiceComponent implements OnInit {
     this.phoneForms.push(phone);
 
   }
+  getrate() {
+    this.userService.getrate(localStorage.getItem('SuperAdmin')).subscribe(data => {
+      let result: any = {}
+      result = data;
+      this.ratelist = result.result
 
+    },
+      error => {
+        console.log(error);
+
+      })
+  }
   deletePhone(i) {
     this.phoneForms.removeAt(i)
   }
@@ -185,40 +284,41 @@ export class InvoiceComponent implements OnInit {
       userId: localStorage.getItem('userId'),
     }
     console.log(data);
-    this.userService.creatinvoice(data).subscribe(result=>{
+    this.userService.creatinvoice(data).subscribe(result => {
       console.log(data);
       this.toastr.success('Awesome!', 'invoice saved suceesfully');
       this.createjournal()
     },
-    err=>{
-      console.log(err);
-      this.toastr.error('Error in invoice!', 'Server Error')
+      err => {
+        console.log(err);
+        this.toastr.error('Error in invoice!', 'Server Error')
 
-    })
+      })
   }
-  onChangeObj(data){
-    var account:any;
-  for(var i=0;i<this.contactlist.length;i++){
-    if(this.contactlist[i]._id==data){
-      // parent=this.contactlist[i].contact_type;
-      account=this.contactlist[i].name;
-      break;
+  onChangeObj(data) {
+    var account: any;
+    for (var i = 0; i < this.contactlist.length; i++) {
+      if (this.contactlist[i]._id == data) {
+        // parent=this.contactlist[i].contact_type;
+        account = this.contactlist[i].name;
+        this.customername = this.contactlist[i].name;
+        break;
+      }
     }
-  }
 
-    let datas={
-      accounttype:"Asset",
-      account:account,
-      parent:"Account Receivables",
-      super_parent_Account:"Current Assets",
-      superAdminId:localStorage.getItem('SuperAdmin')
+    let datas = {
+      accounttype: "Asset",
+      account: account,
+      parent: "Account Receivables",
+      super_parent_Account: "Current Assets",
+      superAdminId: localStorage.getItem('SuperAdmin')
     }
-  console.log(datas)
+    console.log(datas)
     this.userService.accountbytype(datas).subscribe(result => {
       console.log(result);
-      let something:any;
-      something=result
-      this.firstaccountid=something.result[0]._id
+      let something: any;
+      something = result
+      this.firstaccountid = something.result[0]._id
       console.log(this.firstaccountid)
     },
       err => {
@@ -227,67 +327,67 @@ export class InvoiceComponent implements OnInit {
       })
   }
 
-  get(){
-  
-    let datas={
-      accounttype:"Revenue",
-      account:"Fleets",
-     
-      superAdminId:localStorage.getItem('SuperAdmin')
-    }
-  
-      this.userService.accountbyname(datas).subscribe(result => {
-        console.log(result);
-        let something:any;
-        something=result
-        if(something.result.length!=0){
-          this.fleetaccount=something.result[0]._id
-        }
-        console.log('fleet account',this.fleetaccount)
-      },
-        err => {
-          console.log(err)
-  
-        })
+  get() {
+
+    let datas = {
+      accounttype: "Revenue",
+      account: "Fleets",
+
+      superAdminId: localStorage.getItem('SuperAdmin')
     }
 
+    this.userService.accountbyname(datas).subscribe(result => {
+      console.log(result);
+      let something: any;
+      something = result
+      if (something.result.length != 0) {
+        this.fleetaccount = something.result[0]._id
+      }
+      console.log('fleet account', this.fleetaccount)
+    },
+      err => {
+        console.log(err)
 
-  createjournal(){
-    
-    let data={
-      date:new Date().toISOString(),
-      reference:this.myForm.value.customer,
-      notes:'',
-      total:this.myForm.value.amount,
-      userId:localStorage.getItem('userId'),
-      journal_base:'bank',
-      detail:[{
-        accountId:this.firstaccountid,
-        debit:this.myForm.value.amount,
-        description:'description'
+      })
+  }
+
+
+  createjournal() {
+
+    let data = {
+      date: new Date().toISOString(),
+      reference: this.myForm.value.customer,
+      notes: '',
+      total: this.myForm.value.amount,
+      userId: localStorage.getItem('userId'),
+      journal_base: 'bank',
+      detail: [{
+        accountId: this.firstaccountid,
+        debit: this.myForm.value.amount,
+        description: 'description'
       },
       {
-        accountId:this.fleetaccount,
-        credit:this.myForm.value.amount,
-        description:'description'
+        accountId: this.fleetaccount,
+        credit: this.myForm.value.amount,
+        description: 'description'
       }
-    ]
-  }
-  console.log(data);
+      ]
+    }
+    console.log(data);
 
-  this.userService.journalcreat(data).subscribe(result => {
-    console.log(result);
-    this.toastr.success('Awesome!', 'Journal created suceesfully');
-    console.log(result);
-    this.SharedService.abc('invoice');
-   
-  },
-    err => {
-      console.log(err)
-      this.toastr.error('Error!', 'Server Error')
+    this.userService.journalcreat(data).subscribe(result => {
+      console.log(result);
+      this.toastr.success('Awesome!', 'Journal created suceesfully');
+      console.log(result);
+      this.SharedService.abc('invoice');
 
-    })
+    },
+      err => {
+        console.log(err)
+        this.toastr.error('Error!', 'Server Error')
+
+      })
   }
-  
-  
+
+
 }
