8a02a86ba2087434d82d51c40df4938ed253f059
mefy-brij <brij.singh@mefy.care>
2019-09-02 16:53:32 +0530

    a

--- src/app/UrlConfig.js
+++ src/app/UrlConfig.js
@@ -2,5 +2,5 @@
 
 
 // export const APIURL =  'http://ec2-52-66-250-144.ap-south-1.compute.amazonaws.com:4052/'
-export const APIURL =  'ec2-13-234-223-22.ap-south-1.compute.amazonaws.com:4052/'
+export const APIURL =  'http://ec2-13-234-223-22.ap-south-1.compute.amazonaws.com:4052/'
 
